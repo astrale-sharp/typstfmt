ca97ac6420b44fcc433b67dfd86cd65ff7283e21
Astrale_works <ash4567@outlook.fr>
2023-03-25 15:38:55 +0100

    after publish

--- cli/target/package/typst-fmt-cli-0.1.0/target/debug/deps/typst_fmt_cli-9fbabd1f1a3ff90c.d
+++
@@ -1,5 +0,0 @@
-/home/astrale/Coding/rust/typst-fmt/cli/target/package/typst-fmt-cli-0.1.0/target/debug/deps/typst_fmt_cli-9fbabd1f1a3ff90c: src/main.rs
-
-/home/astrale/Coding/rust/typst-fmt/cli/target/package/typst-fmt-cli-0.1.0/target/debug/deps/typst_fmt_cli-9fbabd1f1a3ff90c.d: src/main.rs
-
-src/main.rs:
--- cli/target/package/typst-fmt-cli-0.1.0/target/debug/typst-fmt-cli.d
+++
@@ -1,1 +0,0 @@
-/home/astrale/Coding/rust/typst-fmt/cli/target/package/typst-fmt-cli-0.1.0/target/debug/typst-fmt-cli: /home/astrale/Coding/rust/typst-fmt/cli/target/package/typst-fmt-cli-0.1.0/src/main.rs
