09244cd5abd706fbeee4745b8482f3f99430feca
Astrale_works <ash4567@outlook.fr>
2023-03-26 12:02:55 +0200

    adds similar asserts, adds a big test

--- src/lib.rs
+++ src/lib.rs
@@ -77,45 +77,69 @@ mod tests {
 
     #[test]
     fn test_name() {
-        assert_eq!(format_with_rules("#{ }", &[SpaceRule.as_dyn()]), "#{ }");
+        similar_asserts::assert_eq!(format_with_rules("#{ }", &[SpaceRule.as_dyn()]), "#{ }");
     }
 
     #[test]
     fn two_spaces_become_one() {
-        assert_eq!(format_with_rules("#{  }", &[SpaceRule.as_dyn()]), "#{ }");
+        similar_asserts::assert_eq!(format_with_rules("#{  }", &[SpaceRule.as_dyn()]), "#{ }");
+    }
+
+    #[test]
+    fn complex() {
+        let expected = 
+r##"#import "template.typ": *
+#show: letter.with(
+    sender: [
+        Jane Smith, 
+        Universal Exports, 
+        1 Heavy Plaza, 
+        Morristown, 
+        NJ 07964,
+    ],
+    recipient: [
+        Mr. John Doe \
+        Acme Corp. \
+        123 Glennwood Ave \
+        Quarto Creek, VA 22438
+    ],
+    date: [
+        Morristown, 
+        June 9th, 2023,
+        ],
+    subject: [
+        test
+        ],
+    name: [
+        Jane Smith \
+        Regional Director
+        ],
+)
+
+Dear Joe,
+
+#lorem(99)
+
+Best,"##;
+    let input = 
+r##"#import "template.typ": *
+#show: letter.with(sender:[Jane Smith, Universal Exports, 1 Heavy Plaza, Morristown, NJ 07964,],recipient: [Mr. John Doe \ Acme Corp. \ 123 Glennwood Ave \ Quarto Creek, VA 22438],date: [Morristown, June 9th, 2023,],subject: [test],name: [Jane Smith \Regional Director],)
+
+Dear Joe,
+
+#lorem(99)
+
+Best,
+"##;
+    similar_asserts::assert_eq!(typst_format(input), expected);
+
     }
 }
 
-// const SOURCE1: &str = r##"
-// #import "template.typ":*"##;
-
-// const SOURCE: &str = r##"
-// #import "template.typ": *
-// #show: letter.with(
-//   sender: [
-//     Jane Smith, Universal Exports, 1 Heavy Plaza, Morristown, NJ 07964
-//   ],
-//   recipient: [
-//     Mr. John Doe \
-//     Acme Corp. \
-//     123 Glennwood Ave \
-//     Quarto Creek, VA 22438
-//   ],
-//   date: [Morristown, June 9th, 2023],
-//   subject: [test],
-//   name: [Jane Smith \ Regional Director],
-// )
-
-// Dear Joe,
-
-// #lorem(99)
-
-// Best,
-// "##;
 
 /// rules :
 /// ModuleImport, space after colon
-/// ImportItems : no trailing comma
+/// ImportItems : trailing comma
 
 #[test]
 fn feature() {
