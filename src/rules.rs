2d634f7ba894cf682d75a074f9aec92ec1dfae08
Astrale_works <ash4567@outlook.fr>
2023-03-29 11:10:07 +0200

    adds a failing test for function need more context to treat

--- src/rules.rs
+++ src/rules.rs
@@ -278,6 +278,15 @@ mod tests {
         }
 
         #[test]
+        fn reduce_space() {
+            init();
+            similar_asserts::assert_eq!(
+                format_with_rules("#{f1(\n\n1,\n\n2,\n\n3,\n)}", &[IdentItemFunc.as_dyn()]),
+                format!("#{{f1(\n{0}1,\n{0}2,\n{0}3,\n{0})}}", " ".repeat(4))
+            );
+        }
+
+        #[test]
         fn nested_func() {
             init();
             similar_asserts::assert_eq!(
