df937a0190050bc0872f72ff551beb4b910717d7
Astrale_works <ash4567@outlook.fr>
2023-04-16 10:28:56 +0200

    adds ignored tests

--- src/rules/tests/ident_item_func.rs
+++ src/rules/tests/ident_item_func.rs
@@ -1,6 +1,23 @@
 use super::*;
 
 test_snippet!(
+    no_apply_if_not_in_code_block,
+    ignore = "unimplemented",
+    expect = "#f()",
+    "#f()",
+    &[IdentItemFunc.as_dyn()]
+);
+
+
+test_snippet!(
+    no_change_if_no_args,
+    ignore = "unimplemented",
+    expect = "#{f()}",
+    "#{f()}",
+    &[IdentItemFunc.as_dyn()]
+);
+
+test_snippet!(
     basic_func,
     expect = format!("#{{f1(\n{0}1,\n{0}2,\n{0}3,\n)}}", " ".repeat(4)),
     "#{f1(1,2,3,)}",
--- src/rules/tests/typst_format.rs
+++ src/rules/tests/typst_format.rs
@@ -1,26 +1,12 @@
 use super::*;
 
-test_snippet!(
-    test_eof,
-    ignore = "might get implemented another way",
-    expect = r"#{}",
-    "#{} \n",
-    rules().as_slice()
-);
-test_snippet!(
-    test_eof_1,
-    ignore = "might get implemented another way",
-    expect = r"#{}",
-    "#{} \n ",
-    rules().as_slice()
-);
-test_snippet!(test_eof_2, expect = r"#{}", r"#{}", rules().as_slice());
+mod eof;
 
 test_snippet!(
-    test_eof_3,
-    ignore = "might get implemented another way",
-    expect = r"#{}",
-    r"#{} ",
+    later,
+    ignore = "need to treat fn first",
+    expect = "#{\n    a()[]\n}", 
+    "#{\na()[]\n}", 
     rules().as_slice()
 );
 
