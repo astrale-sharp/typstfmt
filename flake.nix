23402fe85d9273c44a15d6708a493ec83388eb51
BattleCh1cken <BattleCh1cken@larkov.de>
2023-07-31 12:21:36 -0400

    Moved to crane for nix build

--- flake.nix
+++ flake.nix
@@ -4,57 +4,50 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-
-      perSystem =
-        { config
-        , pkgs
-        , system
-        , ...
-        }: {
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [ config.packages.typstfmt ];
-            packages = with pkgs; [
-              cargo
-              clippy
-              pre-commit
-              rust-analyzer
-              rustc
-              rustfmt
-              rustPackages.clippy
-            ];
-
-            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
-          };
-
-          packages =
-            {
-              typstfmt = pkgs.rustPlatform.buildRustPackage {
-                name = "typstfmt";
-
-                src = ./.;
-
-                cargoLock = {
-                  lockFile = ./Cargo.lock;
-                  outputHashes."typst-syntax-0.6.0" = "sha256-oAn783W7P8zY8gqPyy/w1goW/tdLJgf0qm2qAEJ9Vto=";
-                };
-
-                nativeBuildInputs = with pkgs; [ pkg-config cargo-insta ];
-
-                hash = "sha256-oAn783W7P8zY8gqPyy/w1goW/tdLJgf0qm2qAEJ9Vto=";
-              };
-            }
-            // { default = config.packages.typstfmt; };
-
-          formatter = pkgs.alejandra;
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
         };
-    };
+
+        craneLib = crane.lib.${system};
+        typstfmt =
+          craneLib.buildPackage
+            {
+              src = craneLib.path ./.;
+
+              buildInputs = [
+                pkgs.cargo-insta
+              ];
+            };
+      in
+      {
+        checks = {
+          inherit typstfmt;
+        };
+
+        packages.default = typstfmt;
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = typstfmt;
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.checks.${system};
+
+          nativeBuildInputs = with pkgs; [
+            cargo
+            rustc
+          ];
+        };
+      });
 }
