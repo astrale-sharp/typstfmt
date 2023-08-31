0b9cb9a3d0eece30d4a4a8346c34b6187ff673e0
Astrale <ash4567@outlook.fr>
2023-08-31 11:16:40 +0200

    renames some test

--- lib/src/tests/basic.rs
+++ lib/src/tests/basic.rs
@@ -7,15 +7,15 @@ make_test!(let_stmt_unchanged, "#let ident = variable");
 make_test!(let_stmt_period_terminated, "#let ident = variable;");
 make_test!(let_stmt_no_spacing, "#let ident=variable");
 make_test!(
-    content_block_spaced,
+    content_block_basic,
     "#[\n_Glaciers_ form an important part \nof the earth's climate system.]"
 );
 make_test!(ten_adds, &format!("#{{{}1}}", "1+".repeat(10)));
 make_test!(thirty_adds, &format!("#{{{}1}}", "1+".repeat(30)));
-make_test!(markup_block_spaced, "[ 4 ]");
-make_test!(markup_block_too_spaced, "[  4  ]");
-make_test!(markup_block_tight, "[4]");
-make_test!(markup_block_only_space, "[   ]");
+make_test!(content_block_spaced, "[ 4 ]");
+make_test!(content_block_too_spaced, "[  4  ]");
+make_test!(content_block_tight, "[4]");
+make_test!(content_block_only_space, "[   ]");
 make_test!(just, "#let page_turned = page not in header_pages");
 make_test!(short_if_else, "#if true{}else {}");
 make_test!(
