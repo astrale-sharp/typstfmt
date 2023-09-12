4673e50874a741807718537c325ef068ed609cfb
Astrale <ash4567@outlook.fr>
2023-09-12 15:03:44 +0200

    better future handling of config + line_wrap option (not used yet)

--- lib/src/config.rs
+++ lib/src/config.rs
@@ -1,11 +1,14 @@
 use serde::Deserialize;
 use serde::Serialize;
 #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+#[serde(default)] 
+#[serde(deny_unknown_fields)] 
 pub struct Config {
     pub indent_space: usize,
     pub max_line_length: usize,
     /// If enabled, when breaking arguments, it will try to keep more on one line.
     pub experimental_args_breaking_consecutive: bool,
+    pub line_wrap: bool
 }
 
 impl Default for Config {
@@ -14,6 +17,7 @@ impl Default for Config {
             // this being strictly > to 1 is assumed.
             indent_space: 2,
             max_line_length: 80,
+            line_wrap: true,
             experimental_args_breaking_consecutive: false,
         }
     }
