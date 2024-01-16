44a8d296eb4e45179a48773667bf4edd6fdd4583
Astrale <ash4567@outlook.fr>
2024-01-16 22:50:52 +0100

    various visit improvments

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -352,106 +352,120 @@ fn preserve_pass(node: &mut FmtNode) -> PreserveData {
     }
 }
 
-fn visit_content_block(parent: &LinkedNode<'_>, w: &mut Writer<'_>) {
-    // TODO put marker to come back here if too long
+fn visit_content_block(node: &FmtNode<'_>, w: &mut Writer<'_>) {
+    let Content::Children(c) = &node.content else {
+        unreachable!()
+    };
+    let mut c = c.iter().filter(|c| c.kind != FmtKind::Space);
+    assert!(matches_text(c.next(), "["));
+    assert!(matches_text(c.last(), "]"));
     w.push_str("[");
-    // TODO ADD MARK IDENT
-    visit_markup(parent, w) // todo try markup_short then markup_long
-                            // TODO ADD MARK DEDENT
-                            // TODO Check from marker if too long : markup_long.
-}
-
-fn visit_code_block(parent: &LinkedNode<'_>, w: &mut Writer<'_>) {
     w.mark_indent();
-    todo!();
+    visit_markup(node, w);
     w.mark_dedent();
+    w.push_str("]");
 }
 
-fn visit_code(parent: &LinkedNode<'_>, w: &mut Writer<'_>) {
-    todo!()
+fn visit_code_block(node: &FmtNode<'_>, w: &mut Writer<'_>) {
+    let Content::Children(c) = &node.content else {
+        unreachable!()
+    };
+    let mut c = c.iter().filter(|c| c.kind != FmtKind::Space);
+    assert!(matches_text(c.next(), "{"));
+    assert!(matches_text(c.last(), "}"));
+    // todo put a mark try one line code else rewind
+
+    w.push_str("{");
+    w.mark_indent();
+    visit_code(node, w);
+    w.mark_dedent();
+    w.push_str("}");
 }
 
-fn visit_set_rule(parent: &LinkedNode<'_>, w: &mut Writer<'_>) {
-    todo!()
+fn matches_text(c: Option<&FmtNode>, s: &str) -> bool {
+    matches!(c.map(|c| &c.content), Some(Content::Text(t)) if t == &s)
 }
 
-fn visit_show_rule(parent: &LinkedNode<'_>, w: &mut Writer<'_>) {
-    todo!()
-}
-
-fn visit_math(parent: &LinkedNode<'_>, w: &mut Writer<'_>) {
-    todo!()
-}
-
-fn visit_equation(parent: &LinkedNode<'_>, w: &mut Writer<'_>) {
-    todo!()
-}
-
-fn visit_conditional(parent: &LinkedNode<'_>, w: &mut Writer<'_>) {
-    todo!()
-}
-
-fn visit_while(parent: &LinkedNode<'_>, w: &mut Writer<'_>) {
-    todo!()
-}
-
-fn visit_for(parent: &LinkedNode<'_>, w: &mut Writer<'_>) {
-    todo!()
-}
-
-fn visit_import(parent: &LinkedNode<'_>, w: &mut Writer<'_>) {
-    todo!()
-}
-
-fn visit_include(parent: &LinkedNode<'_>, w: &mut Writer<'_>) {
-    todo!()
-}
-
-/// makes sure it's on one line
-fn visit_heading(parent: &LinkedNode<'_>, w: &mut Writer<'_>) {
-    todo!("headings")
-}
-
-fn visit_markup(parent: &LinkedNode<'_>, w: &mut Writer<'_>) {
-    debug_assert!(parent.is::<typst_syntax::ast::Markup>());
-    for parent in parent.children() {
-        match parent.kind() {
-            CodeBlock => visit_code_block(&parent, w),
-            ContentBlock => visit_content_block(&parent, w),
-            Equation => visit_equation(&parent, w),
-            Parbreak => {
-                w.new_line();
-                w.new_line();
-            }
-            Raw => visit_raw(&parent, w),
-            Heading => visit_heading(&parent, w),
-            Hashtag => {
-                w.push_node(&parent);
-                visit_code(&parent, w)
-            }
-
-            TermItem | TermMarker | HeadingMarker | ListItem | ListMarker | EnumItem | Math
-            | MathIdent | MathAlignPoint | MathDelimited | MathAttach | MathPrimes | MathFrac
-            | MathRoot | EnumMarker => {
-                unreachable!()
-            }
-            LineComment => {
-                w.push_node(&parent);
-                w.new_line();
-            }
-            BlockComment => w.push_node(&parent),
-            Space => (),
-            _ => {
-                w.push_node_spaced(&parent);
-            }
+fn visit_markup(node: &FmtNode<'_>, w: &mut Writer<'_>) {
+    // debug_assert!(node.is::<typst_syntax::ast::Markup>());
+    let Content::Children(children) = &node.content else {
+        unreachable!()
+    };
+    for node in children {
+        match &node.kind {
+            FmtKind::Markup => visit_markup(node, w),
+            FmtKind::ContentBlock => visit_content_block(node, w),
+            FmtKind::FuncCall => visit_func_call(node, w),
+            FmtKind::ParamsLike => visit_params_like(node, w, false),
+            FmtKind::ParamsLikeParenthesized => visit_params_like(node, w, true),
+            FmtKind::Binary => visit_binary(node, w),
+            FmtKind::Unary => visit_unary(node, w),
+            FmtKind::Preserve(_) | FmtKind::Comment => visit_preserve(node, w),
+            FmtKind::Space => (),
+            FmtKind::OneLineMarkup => visit_one_line_markup(node, w),
+            FmtKind::Conditional => visit_conditional(node, w),
+            FmtKind::Parbreak => visit_parbreak(node, w),
+            FmtKind::Code => visit_code(node, w),
+            FmtKind::CodeBlock => visit_code_block(node, w),
+            FmtKind::Math => visit_math(node, w),
+            FmtKind::Equation => visit_equation(node, w),
+            FmtKind::WithSpacing(s) => visit_spacing(s, node, w),
         }
     }
 }
 
-fn visit_raw(parent: &LinkedNode<'_>, w: &mut Writer<'_>) {
-    let m = w.mark_preserve();
-    w.push_node(&parent)
-    // w no indent close
+fn visit_spacing(s: &Spacing, node: &FmtNode<'_>, w: &mut Writer<'_>) {
+    todo!()
+}
+
+fn visit_equation(node: &FmtNode<'_>, w: &mut Writer<'_>) {
+    todo!()
+}
+
+fn visit_math(node: &FmtNode<'_>, w: &mut Writer<'_>) {
+    todo!()
+}
+
+fn visit_code(node: &FmtNode<'_>, w: &mut Writer<'_>) {
+    todo!()
+}
+
+fn visit_parbreak(node: &FmtNode<'_>, w: &mut Writer<'_>) {
+    todo!()
+}
+
+fn visit_conditional(node: &FmtNode<'_>, w: &mut Writer<'_>) {
+    todo!()
+}
+
+fn visit_one_line_markup(node: &FmtNode<'_>, w: &mut Writer<'_>) {
+    todo!()
+}
+
+fn visit_preserve(node: &FmtNode<'_>, w: &mut Writer<'_>) {
+    todo!()
+}
+
+fn visit_unary(node: &FmtNode<'_>, w: &mut Writer<'_>) {
+    todo!()
+}
+
+fn visit_binary(node: &FmtNode<'_>, w: &mut Writer<'_>) {
+    todo!()
+}
+
+fn visit_params_like(node: &FmtNode<'_>, w: &mut Writer<'_>, parenthesized: bool) {
+    todo!()
+}
+
+fn visit_func_call(node: &FmtNode<'_>, w: &mut Writer<'_>) {
+    let Content::Children(children) = &node.content else {
+        unreachable!()
+    };
+    let mut children = children.iter();
+    // todo, more checks? like is it an ident etc
+    w.push_node(children.next().unwrap());
+    visit_params_like(children.next().unwrap(), w, false)
 }
 
 #[test]
