92db83e2fd7c7b19660acfc7a1692dc1d4785ed0
Astrale <ash4567@outlook.fr>
2024-01-16 22:46:13 +0100

    adds and test briefly a preserve pass use && imports shenanigans

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -9,19 +9,18 @@
 //     clippy::dbg_macro,
 // )]
 
+use std::fmt::Debug;
 use std::rc::Rc;
 
-use itertools::Itertools;
-use typst_syntax::SyntaxKind::*;
 use typst_syntax::{parse, LinkedNode};
-
 mod config;
 pub use config::Config;
 
 mod writer;
 use writer::Writer;
 
-use crate::writer::MarkKind;
+mod spacing;
+use spacing::*;
 
 mod utils;
 // mod tests;
@@ -39,6 +38,8 @@ pub fn format(s: &str, config: Config) -> String {
     let mut s = String::new();
     let mut context = Writer::new(config, &mut s);
     let root = LinkedNode::new(&init);
+    let mut root = convert(root, None);
+    preserve_pass(&mut root);
     visit_markup(&root, &mut context);
     regex::Regex::new("( )+\n")
         .unwrap()
@@ -136,111 +137,83 @@ pub fn convert<'a>(node: LinkedNode<'a>) -> FmtNode<'a> {
             FmtKind::SpaceAfter.with_children(node)
         }
 
-        Heading => FmtKind::OnLineMarkup.with_children(node),
+/// preserve index and parent must handle me flags
+#[derive(Default)]
+struct PreserveData {
+    preserve_idx: i32,
+    parent_must_handle: bool,
+}
 
-        ListItem | EnumItem | TermItem => FmtKind::ListLike.with_children(node),
-        // todo sort the rest of those.
-        Equation => FmtKind::Equation.with_children(node),
-        Math => todo!(),
-        MathIdent => todo!(),
-        MathAlignPoint => todo!(),
-        MathDelimited => todo!(),
-        MathAttach => todo!(),
-        MathPrimes => todo!(),
-        MathFrac => todo!(),
-        MathRoot => todo!(),
-        Hashtag => todo!(),
-        LeftBrace => todo!(),
-        RightBrace => todo!(),
-        LeftBracket => todo!(),
-        RightBracket => todo!(),
-        LeftParen => todo!(),
-        RightParen => todo!(),
-        Comma => todo!(),
-        Semicolon => todo!(),
-        Colon => todo!(),
-        Star => todo!(),
-        Underscore => todo!(),
-        Dollar => todo!(),
-        Plus => todo!(),
-        Minus => todo!(),
-        Slash => todo!(),
-        Hat => todo!(),
-        Prime => todo!(),
-        Dot => todo!(),
-        Eq => todo!(),
-        EqEq => todo!(),
-        ExclEq => todo!(),
-        Lt => todo!(),
-        LtEq => todo!(),
-        Gt => todo!(),
-        GtEq => todo!(),
-        PlusEq => todo!(),
-        HyphEq => todo!(),
-        StarEq => todo!(),
-        SlashEq => todo!(),
-        Dots => todo!(),
-        Arrow => todo!(),
-        Root => todo!(),
-        Not => todo!(),
-        And => todo!(),
-        Or => todo!(),
-        None => todo!(),
-        Auto => todo!(),
-        Let => todo!(),
-        Set => todo!(),
-        Show => todo!(),
-        If => todo!(),
-        Else => todo!(),
-        For => todo!(),
-        In => todo!(),
-        While => todo!(),
-        Break => todo!(),
-        Continue => todo!(),
-        Return => todo!(),
-        Import => todo!(),
-        Include => todo!(),
-        As => todo!(),
-        Code => todo!(),
-        Ident => todo!(),
-        Bool => todo!(),
-        Int => todo!(),
-        Float => todo!(),
-        Numeric => todo!(),
-        Str => todo!(),
-        CodeBlock => todo!(),
-        ContentBlock => todo!(),
-        Parenthesized => todo!(),
-        Array => todo!(),
-        Dict => todo!(),
-        Named => todo!(),
-        Keyed => todo!(),
-        Unary => todo!(),
-        Binary => todo!(),
-        FieldAccess => todo!(),
-        FuncCall => todo!(),
-        Args => todo!(),
-        Spread => todo!(),
-        Closure => todo!(),
-        Params => todo!(),
-        LetBinding => todo!(),
-        SetRule => todo!(),
-        ShowRule => todo!(),
-        Conditional => todo!(),
-        WhileLoop => todo!(),
-        ForLoop => todo!(),
-        ModuleImport => todo!(),
-        ImportItems => todo!(),
-        ModuleInclude => todo!(),
-        LoopBreak => todo!(),
-        LoopContinue => todo!(),
-        FuncReturn => todo!(),
-        Destructuring => todo!(),
-        DestructAssignment => todo!(),
-        LineComment => todo!(),
-        BlockComment => todo!(),
-        Error => todo!(),
-        Eof => todo!(),
+impl PreserveData {
+    fn new(preserve_idx: i32, parent_must_handle: bool) -> Self {
+        Self {
+            preserve_idx,
+            parent_must_handle,
+        }
+    }
+}
+
+/// Modifies the tree to isolate preserve nodes
+/// of `// typstfmt::off` matching `// typstfmt::on`
+///
+/// The boolean returned is a preserve flag, the level of preserved nesting.
+///
+/// Do we need to be able to say I handled my children
+///
+/// Example:
+/// let our snippet correspond to
+/// ```ignore
+/// f(
+///     // t::off
+///     f([text  text  text]),2,
+///  
+/// )
+/// // t::on
+/// ```
+///
+/// Here, `f([text  text  text])` wouldn't be tag as Preserve cause it has children
+/// and it's parent is not preserved either so we need else
+///
+fn preserve_pass(node: &mut FmtNode) -> PreserveData {
+    match node.kind {
+        FmtKind::Comment => {
+            let text = node.text().unwrap();
+            if text.contains("typstfmt::off") {
+                return PreserveData::new(1, false);
+            } else if text.contains("typstfmt::on") {
+                return PreserveData::new(-1, false);
+            }
+        }
+        _ => {}
+    }
+
+    match &mut node.content {
+        Content::Text(_) => PreserveData::default(),
+        Content::Children(c) => {
+            let mut node_data = PreserveData::new(0, true);
+            c.iter_mut().for_each(|c| {
+                let child_data = preserve_pass(c);
+                // we ignore typstfmt::on if we weren't preserving already
+                node_data.preserve_idx = (node_data.preserve_idx + child_data.preserve_idx).max(0);
+
+                #[allow(unused_doc_comments)]
+                /// cases: one of my child starts the p process, my parent must not handle me
+                if node_data.preserve_idx > 0 {
+                    // We had things to do so we might have already handles things correctly
+                    node_data.parent_must_handle = false;
+                    match &mut c.content {
+                        Content::Text(_) => c.kind = FmtKind::Preserve(node_data.preserve_idx),
+                        Content::Children(_) => {
+                            if child_data.parent_must_handle || node_data.preserve_idx > 1 {
+                                c.kind = FmtKind::Preserve(node_data.preserve_idx)
+                            }
+                            // else the node handled itself already
+                        }
+                    }
+                }
+            });
+            node_data
+        }
     }
 }
 
@@ -384,6 +357,9 @@ text
     assert!(snippet == expected);
 }
 
+#[cfg(test)]
+use crate::writer::MarkKind;
+
 #[test]
 fn test_preserve() {
     let mut snippet = r#"
@@ -423,4 +399,57 @@ text
     println!("expe: {expected}");
 
     assert!(snippet == expected);
+}
+
+#[test]
+fn test_preserve_pass() {
+    let snippets = [
+        r#"notp // typstfmt::off
+#f(x : [p]) 
+// typstfmt::on
+notp "#,
+        r#"#f([notp], // typstfmt::off
+)
+// typstfmt::off
+// typstfmt::on
+p
+// typstfmt::on
+notp"#,
+        r#"
+// typstfmt::off
+#[
+// typstfmt::off
+#[
+// typstfmt::off
+#[
+// typstfmt::off
+text
+]
+]
+]
+// typstfmt::on
+// typstfmt::on
+// typstfmt::on
+text
+// typstfmt::on
+text
+"#,
+    ];
+
+    for snippet in snippets {
+        let parse = parse(snippet);
+        let root = LinkedNode::new(&parse);
+        println!("===");
+        println!("{snippet}");
+        println!("===");
+        println!("{:?}", root);
+        let mut root = convert(root, None);
+        println!("===");
+        println!("{:?}", root);
+        println!("===");
+        preserve_pass(&mut root);
+        println!("{:?}", root);
+        println!("===");
+        insta::assert_debug_snapshot!(root)
+    }
 }
